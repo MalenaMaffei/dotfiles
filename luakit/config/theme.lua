9bf0f7f0e3f34d51f74438e2f7f1ec12a819e9d9
Anachron <Anachron14@gmx.de>
2016-07-27 13:05:29 +0200

    Adding latest changes

--- luakit/config/theme.lua
+++ luakit/config/theme.lua
@@ -6,64 +6,64 @@ local theme = {}
 
 -- Default settings
 theme.font = "monospace normal 9"
-theme.fg = "#B8C94B"
+theme.fg = "#A7A1A2"
 theme.bg = "#282828"
 
 -- Genaral colours
 theme.success_fg = "#f8f8f8"
-theme.loaded_fg = "#7C8587"
+theme.loaded_fg = "#A7A623"
 theme.error_fg = "#d8d8d8"
 theme.error_bg = "#383838"
 
 -- Warning colours
-theme.warning_fg = "#6A9327"
+theme.warning_fg = "#94702C"
 theme.warning_bg = "#383838"
 
 -- Notification colours
-theme.notif_fg = "#B8C94B"
+theme.notif_fg = "#A7A1A2"
 theme.notif_bg = "#282828"
 
 -- Menu colours
-theme.menu_fg = "#B8C94B"
+theme.menu_fg = "#A7A1A2"
 theme.menu_bg = "#282828"
-theme.menu_selected_fg = "#A4A093"
+theme.menu_selected_fg = "#C8BDB0"
 theme.menu_selected_bg = "#181818"
 theme.menu_title_bg = "#282828"
-theme.menu_primary_title_fg = "#B8C94B"
-theme.menu_secondary_title_fg = "#B8C94B"
+theme.menu_primary_title_fg = "#A7A1A2"
+theme.menu_secondary_title_fg = "#A7A1A2"
 
 -- Proxy manager
-theme.proxy_active_menu_fg = "#B8C94B"
+theme.proxy_active_menu_fg = "#A7A1A2"
 theme.proxy_active_menu_bg = "#282828"
-theme.proxy_inactive_menu_fg = "#B8C94B"
+theme.proxy_inactive_menu_fg = "#A7A1A2"
 theme.proxy_inactive_menu_bg = "#282828"
 
 -- Statusbar specific
-theme.sbar_fg = "#B8C94B"
+theme.sbar_fg = "#A7A1A2"
 theme.sbar_bg = "#282828"
 
 -- Downloadbar specific
-theme.dbar_fg = "#B8C94B"
+theme.dbar_fg = "#A7A1A2"
 theme.dbar_bg = "#282828"
 theme.dbar_error_fg = "#d8d8d8"
 
 -- Input bar specific
-theme.ibar_fg = "#B8C94B"
+theme.ibar_fg = "#A7A1A2"
 theme.ibar_bg = "#282828"
 
 -- Tab label
-theme.tab_fg = "#B8C94B"
+theme.tab_fg = "#A7A1A2"
 theme.tab_bg = "#282828"
 theme.tab_ntheme = "#f8f8f8"
-theme.selected_fg = "#A4A093"
+theme.selected_fg = "#C8BDB0"
 theme.selected_bg = "#181818"
 theme.selected_ntheme = "#f8f8f8"
-theme.loading_fg = "#B8C94B"
+theme.loading_fg = "#A7A1A2"
 theme.loading_bg = "#282828"
 
 -- Trusted/untrusted ssl colours
-theme.trust_fg = "#B8C94B"
-theme.notrust_fg = "#B8C94B"
+theme.trust_fg = "#A7A1A2"
+theme.notrust_fg = "#A7A1A2"
 
 return theme
 -- vim: et:sw=4:ts=8:sts=4:tw=80
